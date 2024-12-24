bdfe006add755a6126864d21d8232b22935ee0aa
Vanshika Singh <vanshikasingh11012003@gmail.com>
2024-12-24 20:51:47 +0530

    initial commit

--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -4,15 +4,14 @@ import { CDN_URL } from "../utils/constants";
 
 const ResturantCard = ({resList}) =>{
     const { cloudinaryImageId, name, cuisines, avgRating, } = resList?.info;
-    return(
-
-        <div className="res-card">
-        <img className="res-logo" src={CDN_URL + cloudinaryImageId}/>
-          <h3>{name}</h3>
-          <h4>{cuisines.join(" , ")} </h4>
-          <h4>{avgRating}</h4>
-          <h4>{resList.info.sla.slaString}</h4>
-        </div>
-    )
+    return (
+      <div className="res-card">
+        <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
+        <h3>Rest Name- {name}</h3>
+        <h4>Cuisines- {cuisines.join(" , ")} </h4>
+        <h4>Rating- {avgRating}</h4>
+        <h4>Making Time-{resList.info.sla.slaString}</h4>
+      </div>
+    );
 }
 export default ResturantCard;
